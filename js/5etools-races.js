be4a905b9af79f39b03d4beca352335fcdfca12d
KadenJantz <kadenjantz@gmail.com>
2025-10-14 01:34:15 -0400

    Race/species feature and speed import

--- js/5etools-races.js
+++ js/5etools-races.js
@@ -169,6 +169,250 @@ function d20plusRaces () {
 				attrs.add(`repeating_proficiencies_${lRowId}_name`, asText);
 				attrs.add(`repeating_proficiencies_${lRowId}_options-flag`, "0");
 			}
+		} else if (d20plus.sheet === "2024") {
+			// Ids
+			const builderId = d20plus.ut.generateRowId();
+			const parentDecisionId = d20plus.ut.generateRowId();
+
+			// References for reuse
+			const current = attrs.character.model.attribs.at(1).attributes.current;
+			const raceSize = (race.size || [Parser.SZ_VARIES]).map(sz => Parser.sizeAbvToFull(sz)).join("/").toUpperCase();
+
+			// CREATE CUSTOM SPECIES
+
+			// Establish builder
+			current.finalize.builderIterations[builderId] = Date.now();
+
+			const decisions = [];
+			
+			// really there seems to be only darkvision for PCs (TODO: Test if this works for other sight types)
+			for (const vision of ["darkvision", "blindsight", "tremorsense", "truesight"]) {
+				if (race[vision]) {
+					const titleCaseSense = vision.charAt(0).toUpperCase() + vision.slice(1);
+					const visionId = "custom-species-" + vision;
+
+					decisions[visionId] = {
+						_id: visionId,
+						parentID: parentDecisionId,
+						recordName: race.name + " | " + titleCaseSense + " | custom-species-" + vision,
+						visible: true,
+						_active: false,
+						metadata: {
+							builderDisplayName: "Custom Species " + titleCaseSense,
+							createdByCustomSpeciesToggle: true,
+							isGeneral: true
+						},
+						payload: {
+							type: "Features",
+							recordName: "Custom Species " + titleCaseSense,
+							name: titleCaseSense,
+							description: "You have " + titleCaseSense + " with a range of " + race[vision] + " feet"
+						},
+						children: '["' + visionId + '-sense"]'
+					};
+
+					decisions[visionId + "-sense"] = attrs.generateSense(race.name, vision, race[vision]);
+				}
+			}
+
+			// Set speed (TODO: Add other movement speeds as integrants and modifiers, or try seperate from custom species)
+			decisions["custom-species-speed"] = {
+				_id: "custom-species-speed",
+				parentID: parentDecisionId,
+				recordName: race.name + " | Speed | custom-species-speed",
+				visible: true,
+				_active: false,
+				metadata: {
+					builderDisplayName: "Custom Species Base Walk Speed",
+					createdByCustomSpeciesSpeed: true,
+					isGeneral: true
+				},
+				payload: {
+					type: "Speed",
+					recordName: "Custom Species Speed",
+					speed: "Walk",
+					calculation: "Set Base",
+					valueFormula: {
+						flatValue: race.speed
+					}
+				},
+				children: "[]"
+			};
+
+			// Set size
+			decisions["custom-species-size"] = {
+				_id: "custom-species-size",
+				parentID: parentDecisionId,
+				recordName: race.name + " | Size | custom-species-size",
+				visible: true,
+				_active: false,
+				metadata: {
+					builderDisplayName: "Custom Species Size",
+					createdByCustomSpeciesSize: true,
+					isGeneral: true,
+					builderDisplayDescription: "Your size is " + raceSize + "."
+				},
+				payload: {
+					type: "Size",
+					recordName: "Custom Species Size",
+					sizeValue: raceSize
+				},
+				children: "[]"
+			};
+
+			// TODO: Add features as decisions using loop
+
+			// Initial decision
+			decisions[parentDecisionId] = {
+				_active: false,
+				_id: parentDecisionId,
+				children: '[]',
+				description: "",
+				metadata: {
+					is2024: false,
+					isCustom: true,
+					hasLocalASI: true,
+					builderDisplayName: race.name
+				},
+				payload: {
+					name: race.name,
+					type: "Species"
+				},
+				recordName: race.name + " | " + parentDecisionId,
+				visible: false
+			};
+
+			// Ensure decisions section exists
+			if (!current["decisions"])
+				current["decisions"] = {};
+			if (!current.decisions["allDecisions"])
+				current.decisions["allDecisions"] = {};
+
+			// Save decisions
+			for (const decision in decisions) {
+				// Initial decision needs to reference all children
+				if (decision.parentID == parentDecisionId)
+					decisions[parentDecisionId].children.push(decision._id);
+
+				current.decisions.allDecisions[decision] = decisions[decision];
+				current.customSpecies.options.allDecisions[decision] = decisions[decision];
+			}
+			current.customSpecies.initialDecision = decisions[parentDecisionId];
+
+			// ADD INTEGRANTS
+			const sourceId = d20plus.ut.generateRowId();
+			const childIds = [];
+
+			// Apply size
+			childIds.push(d20plus.ut.generateRowId());
+			attrs.addIntegrant(childIds[childIds.length-1], {
+				shortID: childIds[childIds.length-1].substring(0,9),
+				name: "Custom Species Size",
+				builderDisplayName: "",
+				_label: "",
+				createdTime: Date.now(),
+				type: "Size",
+				_enabled: true,
+				source: "Species",
+				sourceID: sourceId,
+				childIDs: "[]",
+				parentID: sourceId,
+				overwriteDisabled: false,
+				parentDisabled: false,
+				builderIteration: builderId,
+				recordName: "Custom Species Size",
+				sizeValue: raceSize,
+				arrayPosition: attrs.getIntegrantCount()
+			});
+
+			// Add all features (vision descriptions should be added here, so no need to add that)
+			race.entries.filter(it => it.text).forEach(e => {
+				childIds.push(d20plus.ut.generateRowId());
+				attrs.addIntegrant(childIds[childIds.length-1], {
+					shortID: childIds[childIds.length-1].substring(0,9),
+					name: e.name,
+					builderDisplayName: "",
+					_label: "",
+					createdTime: Date.now(),
+					type: "Features",
+					_enabled: true,
+					source: "Species",
+					sourceID: sourceId,
+					childIDs: "[]",
+					parentID: sourceId,
+					parentDisabled: false,
+					overwriteDisabled: false,
+					builderIteration: builderId,
+					description: e.text,
+					arrayPosition: attrs.getIntegrantCount()
+				});
+			});
+
+			// Apply speed
+			childIds.push(d20plus.ut.generateRowId());
+			attrs.addIntegrant(childIds[childIds.length-1], {
+				shortID: childIds[childIds.length-1].substring(0,9),
+				name: "Custom Species Base Walk Speed",
+				builderDisplayName: "",
+				_label: "",
+				createdTime:  Date.now(),
+				type: "Speed",
+				_enabled: true,
+				source: "Species",
+				sourceID: sourceId,
+				childIDs: "[]",
+				parentID: sourceId,
+				overwriteDisabled: false,
+				parentDisabled: false,
+				builderIteration: builderId,
+				recordName: "Custom Species Speed",
+				speed: "Walk",
+				calculation: "Set Base",
+				valueFormula: { flatValue: race.speed },
+				arrayPosition: attrs.getIntegrantCount()
+			});
+
+			// Apply name to source
+			attrs.addIntegrant(sourceId, {
+				shortID: sourceId.substring(0,9),
+				name: race.name,
+				builderDisplayName: "",
+				_label: "",
+				createdTime:  Date.now(),
+				type: "Species",
+				_enabled: true,
+				source: "Custom",
+				childIDs: childIds,
+				parentID: "",
+				overwriteDisabled: false,
+				parentDisabled: false,
+				builderIteration: builderId,
+				description: "",
+				preventSubspecies: false,
+				arrayPosition: attrs.getIntegrantCount()
+			});
+			
+			attrs.addOrUpdate(`speed`, Parser.getSpeedString(race));
+
+			race.entries.filter(it => it.text).forEach(e => {
+				const fRowId = d20plus.ut.generateRowId();
+				attrs.add(`repeating_traits_${fRowId}_name`, e.name);
+				attrs.add(`repeating_traits_${fRowId}_source`, "Race");
+				attrs.add(`repeating_traits_${fRowId}_source_type`, race.name);
+				attrs.add(`repeating_traits_${fRowId}_description`, e.text);
+				attrs.add(`repeating_traits_${fRowId}_options-flag`, "0");
+				if (race._baseName === "Halfling" && e.name === "Lucky") attrs.addOrUpdate(`halflingluck_flag`, "1");
+			});
+
+			if (race.languageProficiencies && race.languageProficiencies.length) {
+				// FIXME this discards information
+				const profs = race.languageProficiencies[0];
+				const asText = Object.keys(profs).filter(it => it !== "choose").map(it => it === "anyStandard" ? "any" : it).map(it => it.toTitleCase()).join(", ");
+
+				const lRowId = d20plus.ut.generateRowId();
+				attrs.add(`repeating_proficiencies_${lRowId}_name`, asText);
+				attrs.add(`repeating_proficiencies_${lRowId}_options-flag`, "0");
+			}
 		} else if (d20plus.sheet === "shaped") {
 			attrs.addOrUpdate("race", race.name);
 			attrs.addOrUpdate("size", (race.size || [Parser.SZ_VARIES]).map(sz => Parser.sizeAbvToFull(sz)).join("/").toUpperCase());
